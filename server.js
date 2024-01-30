7cfe8887f5db078f4c33df0b3d1b1ff215492f41
jaayydeee <jdoe57679@gmail.com>
2024-01-30 07:58:54 +0100

    new

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json({limit: '50mb'}))
 app.use("/guarantor", guarantorRoutes);
 
 //to serve the frontend
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
+app.use(express.static(path.join(__dirname, "./frontend/dist/index.html")));
 
 app.get("*", function (req, res) {
   res.sendFile(
