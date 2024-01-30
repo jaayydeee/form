f2099d04cd681048714a75d4bcd221bce2f025be
jaayydeee <jdoe57679@gmail.com>
2024-01-30 07:43:52 +0100

    new commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import react from '@vitejs/plugin-react-swc'
+import { plugin } from 'mongoose'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
 })
